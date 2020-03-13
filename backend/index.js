032ffe08a8a0b578671c8f3e86c919de3d18c534
Darren Yeoh <darrenyeoh@192-168-1-109.tpgi.com.au>
2020-03-14 10:46:33 +1100

    CORS policy

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,17 @@ db.once('open', function () {
 
 //Middleware function in express, using this it will only look at an parse incoming json requests
 app.use(express.json());
+
+//CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
+  res.setHeader('Cache-Control', 'no-cache');
+  next();
+ });
+
 app.use('/', routes);
 const server = app.listen(config.PORT, ()=> console.log(`Listening on port ${config.PORT}`))
 
